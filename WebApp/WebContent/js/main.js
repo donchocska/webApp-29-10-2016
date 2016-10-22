655e4c726ec6a6381d85db8f7d23c03fa3e32a72
donchocska <donchobalamjiev@gmail.com>
2016-10-22 23:24:28 +0300

    Initial Checkin

--- WebApp/WebContent/js/main.js
+++ WebApp/WebContent/js/main.js
@@ -1,3 +1,41 @@
-var apples = 5;
-var firstName = "Peter";
-var lastName = "Petrov";
+/*function loginBtnEvent() {
+	$('#login').on('click', userAlert());
+}*/
+
+
+
+/*$('#login').on('click', function(){
+	
+	var userText = $('#username').val();
+	
+	if(userText == ""){
+		$('.user-alert').show();
+	}
+	else{
+		$('.user-alert').hide();
+	}
+	
+})*/
+
+function userAlert() {
+	$('#login').on('click', function(){
+		
+		var userName = $('#username').val();
+		
+		if(userName == ""){
+			$('.user-alert').show();
+			$('.user-alert-short').hide();
+		}
+		else if(userName.length < 6){
+			$('.user-alert').hide();
+			$('.user-alert-short').show();
+		}
+		else{
+			$('.user-alert').hide();
+			$('.user-alert-short').hide();
+			alert("Ok");
+		}
+		
+	})
+	
+}
